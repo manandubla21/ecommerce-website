712854a5b29c9b9e1f20f6f8aab42994d76ee8c0
Manan Dubla <manandubla2109@gmail.com>
2026-04-20 10:38:51 +0530

    Some minor Changes

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <div className='flex flex-col sm:flex-row border-gray-400'>
         {/*Hero left Side */}
-        <div className='w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0'>
+        <div className='w-full sm:w-1/2 flex flex-col items-center justify-center py-10 sm:py-0'>
             <div className='flex items-center gap-2'>
                 <p className='w-8 md:w-11 h-[2px] bg-[#414141]'></p>
                 <p className='font-medium text-sm md:text-base'>Our BestSellers</p>
